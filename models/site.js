b2df292f464616ed552970d651661755a20e660f
Cody Clark <codylcl@yahoo.com>
2022-11-04 10:06:32 -1000

    updated Schema and db collection to include image property

--- models/site.js
+++ models/site.js
@@ -5,6 +5,7 @@ const siteSchema = new mongoose.Schema({
     island: {type: String, required: true},
     category: {type: String, required: true},
     description: {type: String, required: true},
+    image: {type: String, required: true},
     completed: Boolean
 })
 
