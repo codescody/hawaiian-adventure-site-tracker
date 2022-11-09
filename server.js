debb7430dfda4fa5109c3a61bf34f9acbe0b406c
Cody Clark <codylcl@yahoo.com>
2022-11-09 13:07:27 -1000

    added heroku homepage route

--- server.js
+++ server.js
@@ -26,4 +26,7 @@ db.on("error", (err) => console.log(err.message + " is mongo not running?"))
 db.on("connected", () => console.log("mongo connected"))
 db.on("disconnected", () => console.log("mongo disconnected"))
 
+// Heroku reroute
+app.get("/", (req, res) => { res.redirect("/sites") })
+
 app.listen(PORT)
